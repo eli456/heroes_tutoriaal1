74413db3c87d4c03e96f68a3ac7b02b9377a9fa4
Elizabeth Galindo Pedraza <44889628+eli456@users.noreply.github.com>
2022-12-01 15:03:43 -0600

    Develop to master (#16)
    * create project #2
    
    * act code the-hero-editor #3
    
    * add code display-list #4
    
    * add code feature #5
    
    * add code feature #5
    
    * add code add-services #6
    
    * add code add-navigation #7
    
    * add code get-data #12
    
    * add code display-list #4
    
    * add code the-hero-editor #3 (#15)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'heroes_tutoria1';
+  title = 'Tour of Heroes';
 }
